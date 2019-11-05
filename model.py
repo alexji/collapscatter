9e30d0c625acf2b4b392e14dc998d6534870f1de
Alexander Ji <alexji@mit.edu>
2019-11-04 20:53:56 -0800

    Functionized the model and did a quick manual fit of a suitable model

--- model.py
+++ model.py
@@ -36,34 +36,17 @@ class tjet_distr(stats.rv_continuous):
         ii = t > self.tmin
         out[ii] = 1.0 - (t[ii]/self.tmin)**(1-self.alpha)
         return out
-    # alpha=4.2,tmin=13.0
 
-if __name__ == "__main__":
-    fjet = 0.1
-    Mgas = 10**7 # Msun
-    SFE = 0.002 # Ratio of Mstar/Mgas
-    yFe = 0.1 # Msun
-    yEu_rate = 1/1000 * .006 #10**-5 * 0.002 # Msun
-    
-    print("fjet = {}, Mgas = {:.1e}, SFE = {}".format(fjet,Mgas,SFE))
+def run_model(Niter, fjet, Mgas, SFE, yFe, yEu_rate, tmin, alpha):
+    print("Niter={}, fjet = {}, Mgas = {:.1e}, SFE = {}".format(Niter, fjet,Mgas,SFE))
     print("yFe = {}, yEu at 300s = {:.2e}".format(yFe, yEu_rate*300))
     Mstar = Mgas * SFE
     SN_to_Mass = 1/0.009 # Salpeter IMF
     Mass_to_SN = 0.009 # Salpeter IMF
     
-    #N_SN = 100
-    #N_jet = int(N_SN * fjet)
-    
-    
     start = time.time()
-    np.random.seed(2304897)
-    Niter = 1000
-    default_tjet = tjet_distr(tmin=1.)
+    default_tjet = tjet_distr(tmin=tmin,alpha=alpha)
     
-    #MFe = N_SN*yFe
-    #print("[Fe/H] = {:.2f}".format(MFeMH_to_FeH(MFe,Mgas)))
-    #print("Stellar Mass = {:.2e}".format(SN_to_Mass*N_SN))
-    #print("Stellar Mass/Mgas = {:.3f}".format(SN_to_Mass*N_SN/Mgas))
     NSN_array = np.zeros(Niter)
     Njet_array = np.zeros(Niter)
     MFe_array = np.zeros(Niter)
@@ -83,12 +66,70 @@ if __name__ == "__main__":
     EuFe[np.isinf(EuFe)] = EuFe_min
     FeH = MFeMH_to_FeH(MFe_array,Mgas*0.75)
     print("took {:.1f}s".format(time.time()-start))
+
+    return EuFe, FeH, NSN_array, Njet_array
+
+if __name__ == "__main__":
+    fjet0 = 0.01
+    Mgas0 = 10**7 # Msun
+    SFE0 = 0.002 # Ratio of Mstar/Mgas
+    yFe0 = 0.2 # Msun
+    yEu_rate0 = 1/1000 * .006 #10**-5 * 0.002 # Msun
+    tmin0 = 1.0
+    alpha0 = 2.2
+    #EuFe, FeH, NSN_array, Njet_array = run_model(100, fjet, Mgas, SFE, yFe, yEu_rate, tmin, alpha)
+
+    np.random.seed(23459870)
+
+    halo = rd.load_halo()
+    halo["[Ba/Eu]"] = halo["[Ba/H]"] - halo["[Eu/H]"]
+    ii = (pd.notnull(halo["[Eu/Fe]"]) & (~halo["uleu"]) & (halo["[Fe/H]"] < -2.5) & (halo["[Ba/Eu]"] < -0.4) & (~halo["ulba"]))
+    print("Num stars = {}".format(ii.sum()))
+    star_eufe = np.sort(halo["[Eu/Fe]"][ii])
     
-    fig, ax = plt.subplots()
-    ax.hist(EuFe,bins="auto")
-    print("[Fe/H] = {:.2f} +/- {:.2f}".format(np.median(FeH), np.diff(np.percentile(FeH,[50,84]))[0]))
-    print("[Eu/Fe] = {:.2f} +/- {:.2f}".format(np.median(EuFe), np.diff(np.percentile(EuFe,[50,84]))[0]))
-    print("N_SN = {:.2f} +/- {:.2f}".format(np.median(NSN_array), np.diff(np.percentile(NSN_array,[50,84]))[0]))
-    print("N_jet = {:.2f} +/- {:.2f}".format(np.median(Njet_array), np.diff(np.percentile(Njet_array,[50,84]))[0]))
+    def print_stuff(FeH, EuFe, NSN_array, Njet_array):
+        print("[Fe/H] = {:.2f} +/- {:.2f}".format(np.median(FeH), np.diff(np.percentile(FeH,[50,84]))[0]))
+        print("[Eu/Fe] = {:.2f} +/- {:.2f}".format(np.median(EuFe), np.diff(np.percentile(EuFe,[50,84]))[0]))
+        print("N_SN = {:.2f} +/- {:.2f}".format(np.median(NSN_array), np.diff(np.percentile(NSN_array,[50,84]))[0]))
+        print("N_jet = {:.2f} +/- {:.2f}".format(np.median(Njet_array), np.diff(np.percentile(Njet_array,[50,84]))[0]))
+        
+    fig, axes = plt.subplots(2,2,figsize=(8,8))
+    fig.suptitle("fjet={} logMgas={} SFE={} yFe={}\nyEu/t={:.1e} tmin={} alpha={}".format(
+        fjet0, np.log10(Mgas0), SFE0, yFe0, yEu_rate0, tmin0, alpha0))
+    ax = axes[0,0]
+    ax.plot(star_eufe, (np.arange(star_eufe.size)+1)/star_eufe.size, 'k-', label="Stars")
+    for tmin in [0.1, 1, 10]:
+        EuFe, FeH, NSN_array, Njet_array = run_model(100, fjet0, Mgas0, SFE0, yFe0, yEu_rate0, tmin, alpha0)
+        ax.plot(np.sort(EuFe), (np.arange(EuFe.size)+1)/EuFe.size, label="tmin "+str(tmin))
+        print_stuff(FeH, EuFe, NSN_array, Njet_array)
+    ax.legend(); ax.set_xlim(-2,3); ax.set_ylim(0,1)
     
+    ax = axes[0,1]
+    ax.plot(star_eufe, (np.arange(star_eufe.size)+1)/star_eufe.size, 'k-', label="Stars")
+    for fjet in [0.003, 0.01, 0.03]:
+        EuFe, FeH, NSN_array, Njet_array = run_model(100, fjet, Mgas0, SFE0, yFe0, yEu_rate0, tmin0, alpha0)
+        ax.plot(np.sort(EuFe), (np.arange(EuFe.size)+1)/EuFe.size, label="fjet "+str(fjet))
+        print_stuff(FeH, EuFe, NSN_array, Njet_array)
+    ax.legend(); ax.set_xlim(-2,3); ax.set_ylim(0,1)
+    
+    ax = axes[1,0]
+    ax.plot(star_eufe, (np.arange(star_eufe.size)+1)/star_eufe.size, 'k-', label="Stars")
+    for alpha in [1.7, 2.2, 3.2]:
+        EuFe, FeH, NSN_array, Njet_array = run_model(100, fjet0, Mgas0, SFE0, yFe0, yEu_rate0, tmin0, alpha)
+        ax.plot(np.sort(EuFe), (np.arange(EuFe.size)+1)/EuFe.size, label="alpha "+str(alpha))
+        print_stuff(FeH, EuFe, NSN_array, Njet_array)
+    ax.legend(); ax.set_xlim(-2,3); ax.set_ylim(0,1)
+    
+    ax = axes[1,1]
+    ax.plot(star_eufe, (np.arange(star_eufe.size)+1)/star_eufe.size, 'k-', label="Stars")
+    for Mgas in [10**6, 10**7, 10**8]:
+        EuFe, FeH, NSN_array, Njet_array = run_model(100, fjet0, Mgas, SFE0, yFe0, yEu_rate0, tmin0, alpha0)
+        ax.plot(np.sort(EuFe), (np.arange(EuFe.size)+1)/EuFe.size, label="logMgas "+str(np.log10(Mgas)))
+        print_stuff(FeH, EuFe, NSN_array, Njet_array)
+    ax.legend(); ax.set_xlim(-2,3); ax.set_ylim(0,1)
+    
+    fig.tight_layout()
+    fig.subplots_adjust(top=.9)
+    fig.savefig("first_model.png", bbox_inches="tight")
     plt.show()
+    
